a7a5877aa800c16d87e6cd3db81a794d65a60ac1
AisyA72 <b72-2002-arceus@docomo.ne.jp>
2024-10-18 11:48:07 +0900

    add: デプロイ方法

--- index.php
+++ index.php
@@ -1,5 +1,43 @@
 <?php
-  
-  echo "Hello World!";
-  
+
 ?>
+  
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Azureデプロイ解説</title>
+</head>
+<body>
+  <h1>Azureのデプロイ</h1>
+  <!-- デプロイ方法をプログラミングで書いて下さい -->
+  <p>デプロイ方法</p>
+  <ol>
+    <li>GitHubにリポジトリを作成する</li>
+    <li>リポジトリにコードをプッシュする</li>
+    <li>Azureのポータルにログインする</li>
+    <li>「新規リソースの作成」をクリックする</li>
+    <li>「Web App」を選択する</li>
+    <li>「リソースの作成」をクリックする</li>
+    <li>「アプリ名」を入力する</li>
+    <li>「リソースグループ」を選択する</li>
+    <li>「OS」を選択する</li>
+    <li>「App Service プラン」を選択する</li>
+    <li>「App Service プランの作成」をクリックする</li>
+    <li>「作成」をクリックする</li>
+    <li>「デプロイ オプション」をクリックする</li>
+    <li>「ソース」を選択する</li>
+    <li>「GitHub」を選択する</li>
+    <li>「GitHubの認証」をクリックする</li>
+    <li>「リポジトリ」を選択する</li>
+    <li>「ブランチ」を選択する</li>
+    <li>「OK」をクリックする</li>
+    <li>「デプロイ」をクリックする</li>
+    <li>デプロイが完了するまで待つ</li>
+  </ol>
+  <p>以上がAzureのデプロイ方法です。</p>
+
+</body>
+</html>
+
